b4b1b7f97bfa3e558911adce5daea573f9e297ff
onesixromcom <16732145+onesixromcom@users.noreply.github.com>
2019-09-10 01:20:35 +0300

    add html and assets

---
+++ assets/helpers.js
@@ -0,0 +1,54 @@
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function setCookie(name, value, options) {
+  options = options || {};
+
+  var expires = options.expires;
+
+  if (typeof expires == "number" && expires) {
+    var d = new Date();
+    d.setTime(d.getTime() + expires * 1000);
+    expires = options.expires = d;
+  }
+  if (expires && expires.toUTCString) {
+    options.expires = expires.toUTCString();
+  }
+
+  value = encodeURIComponent(value);
+
+  var updatedCookie = name + "=" + value;
+
+  for (var propName in options) {
+    updatedCookie += "; " + propName;
+    var propValue = options[propName];
+    if (propValue !== true) {
+      updatedCookie += "=" + propValue;
+    }
+  }
+
+  document.cookie = updatedCookie;
+}
+
+function hasClass(el, className) {
+  return el.classList ? el.classList.contains(className) : new RegExp('\\b'+ className+'\\b').test(el.className);
+}
+
+function addClass(el, className) {
+  if (el.classList) el.classList.add(className);
+  else if (!hasClass(el, className)) el.className += ' ' + className;
+}
---
+++ assets/script.js
@@ -0,0 +1,62 @@
+/* Init language translator */
+
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'ru',
+    includedLanguages: 'ru,en,es,uk',
+    layout: google.translate.TranslateElement.InlineLayout.SIMPLE,
+    autoDisplay: false,
+    multilanguagePage: true
+  }, 'google_translate_element');
+  detectLanguageAttachClick();
+}
+
+
+function languageSelectorOnclick(e) {
+  e.preventDefault();
+
+  if (hasClass(this, "active")) {
+    return;
+  }
+
+  window.location = this.getAttribute('href');
+  location.reload();
+}
+
+function detectLanguageAttachClick() {
+
+  var lang_current = getCookie('googtrans');
+  var lang_active = 'en';
+
+  // Get langcode from hash.
+  var hash_current = window.location.hash;
+  if (hash_current.indexOf("googtrans") > 0) {
+    lang_current = hash_current.slice(11, 16);
+    lang_current = lang_current.split('|');
+    lang_active = lang_current[lang_current.length - 1];
+  } else if (lang_current.length > 0) {
+    lang_current = lang_current.split('/');
+    lang_active = lang_current[lang_current.length - 1];
+  }
+
+  setCookie('lang_active', lang_active);
+
+  // Attach translation flag to wrapper.
+  var div_wrapper = document.getElementById('wrapper');
+  addClass(div_wrapper, lang_active + '-translated');
+
+  var language_selectors = document.getElementsByClassName('lang-select');
+
+  for (var i = 0; i < language_selectors.length; i++) {
+    // Add class to active language.
+    if (language_selectors[i].getAttribute('data-lang') == lang_active) {
+      addClass(language_selectors[i], 'active');
+      addClass(language_selectors[i].parentElement, 'active');
+    }
+
+    language_selectors[i].onclick = languageSelectorOnclick;
+  }
+  document.getElementsByClassName('lang-select').onclick = languageSelectorOnclick;
+}
+
+
